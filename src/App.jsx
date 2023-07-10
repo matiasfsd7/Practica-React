534cccf17ea498feb9aee9397d73537a2a473df9
matiasfsd7 <matias.n.ortega@gmail.com>
2023-07-10 20:35:01 -0300

    agregue componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { Children } from "react";
 import "./App.css";
+import Titulos from "./Components/titulos";
 
 function App() {
   return (
-    <div>
-      <h1>PRIMERA APP CON REACT</h1>
-    </div>
+    <>
+      <Titulos />
+    </>
   );
 }
+// function Bienvenido(props) {
+//   return <h1>Hola {props.name}, Bienvenido a rollingcode </h1>;
+// }
+// let num = 5;
+// function app2() {
+//   const funsum = (num) => {
+//     retur;
+//     num + 2;
+//   };
+//   const titulo = "HOLA ESTE ES UN TITULO DE PRACTICA";
+//   const contenido = "este es para sumar contenido";
+//   return;
+//   <>
+//     <Children titulo={titulo} contenido={contenido} sum={funsum} />
+//   </>;
+// }
 
 export default App;
---
+++ src/Components/Titulos.jsx
@@ -0,0 +1,13 @@
+const Titulos = () => {
+  // Aqui vamos a cargar toda la logica
+  return (
+    //aqui tambien se puede escribir logica
+    <section>
+      {/* aqui va todo lo maquetado */}
+      <h1>Proyecto contador con react</h1>
+      <h2>Este es un subtitulo</h2>
+    </section>
+  );
+};
+
+export default Titulos;
